cd5db14e714e8b19cbeccaf32dad76379bde1a6c
Pavlo Starobub <starodub.pavlo@alsodev.md>
2026-01-27 15:35:18 +0200

    refactor config to include echo for database engine

--- src/config.py
+++ src/config.py
@@ -7,6 +7,7 @@ class Settings(BaseSettings):
     DB_NAME: str
     DB_USER: str
     DB_PASSWORD: str
+    ECHO: bool
 
     @property
     def DATABASE_URL(self) -> str:
--- src/database.py
+++ src/database.py
@@ -2,6 +2,6 @@ from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from config import settings
 
 
-engine = create_async_engine(settings.DATABASE_URL, echo=True)
+engine = create_async_engine(settings.DATABASE_URL, echo=settings.ECHO)
 
 SessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=engine)
