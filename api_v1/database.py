e511d1b42e0460aa56167e0e9e752ec71628b84c
Pavlo Starobub <starodub.pavlo@alsodev.md>
2026-01-27 18:16:59 +0200

    delete alembic and refactor project structure

--- api_v1/database.py
+++ api_v1/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
-from config import settings
+from api_v1.config import settings
 
 
 engine = create_async_engine(settings.DATABASE_URL, echo=settings.ECHO)
